4b9fe7e246f8e4cc5b2657fbe4ba929c59b9b49a
Michael Davis <michael@daviskingdom.com>
2015-03-25 21:27:21 +0100

    Added two more arrays to track if the "you've been playing too long" messages have been sent during each stage.

--- SISPlugin/src/SISPlugin/Main.php
+++ SISPlugin/src/SISPlugin/Main.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * @author Michael J Davis
+ *
+ */
+
 namespace SISPlugin;
 
 use pocketmine\command\Command;
@@ -18,19 +23,20 @@ use pocketmine\event\player\PlayerInteractEvent;
 use pocketmine\event\player\PlayerKickEvent;
 use pocketmine\event\player\PlayerMoveEvent;
 
-// $playerOnline is a global variable
+// Global Variables
 $playersOnline = array ();
+$playersMsgSentStage1 = array ();
+$playersMsgSentStage2 = array ();
+$stage1MsgSent;
+$stage2MsgSent;
 
 // Global Constants
 define ( "STAGE1_MIN", 12 );
-define ( "STAGE1_MAX", 60 );
-define ( "STAGE2_MIN", 61 );
-define ( "STAGE2_MAX", 120 );
-define ( "STAGE3_MIN", 121 );
-/**
- * @author Michael
- *
- */
+define ( "STAGE2_MIN", 60 );
+define ( "STAGE3_MIN", 120 );
+define ( "STAGE1_MESSAGE", "You have been playing for too long. \n Take a break and eat a a cookie." );
+define ( "STAGE2_MESSAGE", "You have been playing for too long. \n You will be kicked in ONE Minute." );
+
 class Main extends PluginBase implements Listener {
 	/*
 	 * (non-PHPdoc)
@@ -40,25 +46,21 @@ class Main extends PluginBase implements Listener {
 		$this->getLogger ()->info ( "onLoad() has been called!" );
 	}
 	
-	/*
-	 * (non-PHPdoc)
-	 * @see \pocketmine\plugin\PluginBase::onEnable()
-	 */
-	
+
 	/*
 	 * (non-PHPdoc)
 	 * @see \pocketmine\plugin\PluginBase::onEnable()
 	 */
 	public function onEnable() {
-		$this->getLogger ()->info ( "onEnable() has been called!!!" );
-		
+		$this->getLogger ()->info ( "onEnable() has been called!!!" );		
 		$this->getServer ()->getPluginManager ()->registerEvents ( $this, $this );
-		// This line above is required to use additional events
+		// The line above is required to use additional events
 		// Also must include "use pocketmine\event\player\<EventNameToUse>"
 		// You can get supported event names here: http://docs.pocketmine.net/d3/daf/namespacepocketmine_1_1event.html
 		// expand the events on the left column to see the choices. Ex. "use pocketmine\event\player\PlayerJoinEvent"
 	}
 	
+
 	/*
 	 * (non-PHPdoc)
 	 * @see \pocketmine\plugin\PluginBase::onDisable()
@@ -67,14 +69,6 @@ class Main extends PluginBase implements Listener {
 		$this->getLogger ()->info ( "onDisable() has been called!" );
 	}
 	
-	/**
-	 *
-	 * @param $m -
-	 *        	Message text to be sent to all
-	 */
-	public function messageToAll($m) {
-		Server::getInstance ()->broadcastMessage ( $m );
-	}
 	
 	/**
 	 *
@@ -82,7 +76,9 @@ class Main extends PluginBase implements Listener {
 	 *        	- Function to run code when "kicking" a player
 	 */
 	public function onKick(PlayerKickEvent $PKE) {
-		$this->messageToAll ( "someone was kicked" );
+		$p = $PKE->getPlayer ();
+		$name = $p->getDisplayName ();
+		$this->messageToAll ( $name . " was kicked" );
 	}
 	
 	/* (non-PHPdoc)
@@ -92,17 +88,15 @@ class Main extends PluginBase implements Listener {
 		global $playersOnline;
 		switch ($command->getName ()) {
 			case "hi" :
-				$sender->sendMessage ( "Hello " . $sender->getName () . "!!!" . time () );
-				$this->messageToAll ( "Boo Ya" );
-				$e = "Lola140";
+				$sender->sendMessage ( "Hello " . $sender->getName () . "!!!");
+				$sender->sendMessage ( "Boo Ya" );				
 				return true;
 			case "tst" :
 				$sender->sendMessage ( "Test works" );
 				return true;
 			case "sis" :
 				$sender->sendMessage ( " :-)\n  :-)\n   Hello Strothoff School!\n     :-)\n      :-)" );
-				return true;
-			
+				return true;			
 			default :
 				return false;
 		}
@@ -115,18 +109,18 @@ class Main extends PluginBase implements Listener {
 	public function onJoin(PlayerJoinEvent $PJE) {
 		// Format for function is "public function <anyName>(<EventNameFromDocumentation> <anyVariableName>) {
 		global $playersOnline;
+		global $playersMsgSentStage1;
+		global $playersMsgSentStage2;
 		$this->messageToAll ( "onJoin!" );
 		$player = $PJE->getPlayer ();
 		$name = $player->getDisplayName ();
-		$this->getServer ()->broadcastMessage ( "Howdy " . $name . " [DEFAULT] joined the game!!");
+		$this->getServer ()->broadcastMessage ( "Howdy! " . $name . " joined the game!!");
+		
+		// Add the new player and their join time to the $playersOnline Array 
 		
 		$playersOnline [$name] = time ();
-		$playersOnline [$name] = time ();
-		
-		// Duplicate the new entry - for testing only
-		$nameDup = $name . "xx";
-		$playersOnline [$nameDup] = time ();
-		// Duplicate the new entry - for testing only
+		$playersMsgSentStage1 [$name] = null;
+		$playersMsgSentStage2 [$name] = null;
 	}
 	
 	
@@ -135,10 +129,10 @@ class Main extends PluginBase implements Listener {
 	 */
 	public function onQuit(PlayerQuitEvent $PQE) {
 		global $playersOnline;
-		$this->messageToAll ( "bye bye" );
 		$player = $PQE->getPlayer ();
 		$name = $player->getDisplayName ();
 		unset ( $playersOnline [$name] );
+		$this->messageToAll ( "Bye Bye " . $name );
 	}
 	
 		
@@ -153,6 +147,7 @@ class Main extends PluginBase implements Listener {
 	
 	/**
 	 * @param PlayerInteractEvent $PIE
+	 * This is run whenever the player interacts with something i.e. digs a hole, etc.
 	 */
 	public function onTouch(PlayerInteractEvent $PIE) {
 		global $playersOnline;
@@ -164,13 +159,16 @@ class Main extends PluginBase implements Listener {
 	
 	/**
 	 *
-	 * @param PlayerMoveEvent $PME        	
+	 * @param PlayerMoveEvent $PME     
+	 * This is run whenever the player moves   	
 	 */
 	public function onMove(PlayerMoveEvent $PME) {
 		global $playersOnline;
+//		global $playersMsgSentStage1;
+//		global $playersMsgSentStage2;
 		$p = $PME->getPlayer ();
 		$name = $p->getDisplayName ();
-		$this->checkPlayerOnlineTime ( $p, $name );
+		$this->checkPlayerOnlineTime ( $p, $name );	
 	}
 	
 	
@@ -180,22 +178,36 @@ class Main extends PluginBase implements Listener {
 	 *        	p - Handle to the Event to get Player ID
 	 * @param
 	 *        	name - The Display name of the Plaer being checked
+	 *        This fuction checks to see how long a player has been online.  If they have been online longer than the set time(s)
+	 *          a message will be sent reminding them to take a break.
 	 */
 	public function checkPlayerOnlineTime($p, $name) {
 		global $playersOnline;
+		global $stage1MsgSent;
+		global $stage2MsgSent;
+		global $playersMsgSentStage1;
+		global $playersMsgSentStage2;
 		$timeOnline = time () - $playersOnline [$name];
-		$tenSeconds = $timeOnline % 10;		
-		if (($timeOnline >= STAGE1_MIN) and ($timeOnline <= STAGE1_MAX) and $tenSeconds == 0) {
-			$p->sendMessage ( "You have been playing for " . $timeOnline . " seconds." );
-			$p->sendMessage ( "Take a break and eat a cookie" );			
-		} elseif (($timeOnline >= STAGE2_MIN) and ($timeOnline <= STAGE2_MAX)) {
-			
-			$p->sendMessage ( "You have been playing for " . $timeOnline . " seconds." );
-			$p->sendMessage ( "Take a break and eat a cookie" );
-			$p->sendMessage ( "You will be KICKED if you do not" );
+				
+		if (($timeOnline >= STAGE1_MIN) and ($timeOnline <= STAGE2_MIN - 1) and ($playersMsgSentStage1 [$name] != "sent")) {
+			$p->sendMessage (STAGE1_MESSAGE);
+			$playersMsgSentStage1 [$name] = "sent";
+		} elseif (($timeOnline >= STAGE2_MIN) and ($timeOnline <= STAGE3_MIN - 1) and ($playersMsgSentStage2 [$name] != "sent")) {			
+			$p->sendMessage (STAGE2_MESSAGE);
+			$playersMsgSentStage2 [$name] = "sent";
 		} elseif (($timeOnline > STAGE3_MIN)) {
 			
-			$p->kick ( "Take a break!" );
+			$p->kick ( "Player was kicked for not taking a break!" );
 		}
+	}
+	
+	
+	/**
+	 *
+	 * @param $m -
+	 *        	Message text to be sent to all
+	 */
+	public function messageToAll($m) {
+		Server::getInstance ()->broadcastMessage ( $m );
 	}
 }
